499e88b46212679f3d71ce95ef72d821201c424d
Minyoung Heo <minyoungheo.heo@gmail.com>
2018-07-03 19:48:42 -0700

    Added sum method

--- Problem31/Solution.java
+++ Problem31/Solution.java
@@ -1,6 +1,14 @@
 public class Solution{
    public static void main(String[] args){
-	    int[] coins = {2,5,10,20,50,100,200};
+      int[] coins = {2,5,10,20,50,100,200};
       int[] quantity = new int[7];
+
+   }
+   public static sum(int[] coins, int[] quantity){
+     int sum = 0;
+     for(int i = 0 ;i<quantity.length;i++){
+       sum+=coins[i]*quantity[i];
+     }
+     return sum;
    }
 }

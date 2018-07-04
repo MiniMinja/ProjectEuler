3929800b5de271a4bfef39e3286f31578a93cec7
Minyoung Heo <minyoungheo.heo@gmail.com>
2018-07-04 07:08:36 -0700

    "re-created the previous changes that were deleted from the merge"

--- Problem31/Solution.java
+++ Problem31/Solution.java
@@ -3,6 +3,28 @@ public class Solution{
    public static void main(String[] args){
 	    int[] coins = {2,5,10,20,50,100,200};
       int[] quantity = new int[7];
+      int count = 1;
+      for(;add(coins,quantity,0);count++);
+      System.out.println(count);
    }
->>>>>>> e7884fc7a007d6dae8eb011b20b520416380b47e
+   public static int sum(int[] coins, int[] quantity){
+     int sum = 0;
+     for(int i = 0;i<quantity.length;i++){
+       sum += coins[i] * quantity[i];
+     }
+     return sum;
+   }
+   public static boolean add(int[] coins, int[] quantity, int k){
+     if(k < quantity.length){
+       if(sum(coins,quantity) + coins[k] <= 200){
+         quantity[k]++;
+         return true;
+       }
+       else{
+         quantity[k] = 0;
+         return add(coins, quantity, k+1);
+       }
+     }
+     return false;
+   }
 }

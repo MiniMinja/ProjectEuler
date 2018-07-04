5c67265a2d6456c0e99633b7e1f97768ba02def5
Minyoung Heo <minyoungheo.heo@gmail.com>
2018-07-04 07:20:55 -0700

    auto pushed by gitfin. Make sure you init.sh next time

--- gitfin.sh
+++ gitfin.sh
@@ -1,5 +1,5 @@
 find . -name "*~" -print0 | xargs -0 rm -rf
 find . -name "*.class" -print0 | xargs -0 rm -rf
 git add .
-git commit -m "auto pushed by gitfin"
+git commit -m "auto pushed by gitfin. Make sure you init.sh next time"
 git push -u origin master
